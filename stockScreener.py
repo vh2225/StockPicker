2dff765ef5a17a3673f009496827dcdb841c2454
vh2225 <vh2225@columbia.edu>
2019-03-20 23:14:04 -0700

    kinda finished. Ready to switch to front-end part

--- stockScreener.py
+++ stockScreener.py
@@ -2,6 +2,7 @@ import argparse as ap
 import datetime
 import json
 import os
+import re
 
 import stockUtils
 
@@ -20,23 +21,48 @@ def stockGetStats(stockSet):
     return stockList, scraped_data
 
 
-def writeStats(stockList, scraped_data):
+def doDiff():
+    regex = re.compile(r'\d+-\d+-\d+$')
+    # get all dirs
+    all_subdirs = sorted([d for d in os.listdir('.') if os.path.isdir(d) and regex.match(d)], reverse=True)
+    last2Runs = [all_subdirs]
+    # filter out the last 2
+    for dir in all_subdirs:
+        if len(last2Runs) == 2:
+            break
+        last2Runs.append(dir)
+    if len(last2Runs) == 2:
+        new = set((line.strip() for line in open(last2Runs[0] + '/stockList.json')))
+        old = set((line.strip() for line in open(last2Runs[1] + '/stockList.json')))
+        with open('diff.txt', 'w') as diff:
+            for line in new:
+                if line not in old:
+                    diff.write('[-] {}\n'.format(line))
+
+            for line in old:
+                if line not in new:
+                    diff.write('[+] {}\n'.format(line))
+
+
+def writeStats(stockList, scraped_data, suffix=''):
     dateStamp = datetime.datetime.now().strftime("%Y-%m-%d")
-    os.makedirs(dateStamp, exist_ok=True)
+    dirName = '{}{}'.format(dateStamp, suffix)
+    os.makedirs(dirName, exist_ok=True)
     script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
     # write companyList
-    compList = "{}/stockList.json".format(dateStamp)
+    compList = "{}/stockList.json".format(dirName, suffix)
     abs_file_path = os.path.join(script_dir, compList)
     with open(abs_file_path, 'w') as fp:
         json.dump(stockList, fp, indent=4)
     # write companyData
-    compData = '{}/stockData.json'.format(dateStamp)
+    compData = '{}/stockData.json'.format(dirName, suffix)
     abs_file_path = os.path.join(script_dir, compData)
     with open(abs_file_path, 'w') as fp:
         json.dump(scraped_data, fp, indent=4)
 
 
-def emailStats(stockList, scraped_data):
+def emailStats(stockList=None, scraped_data=None):
+    doDiff()
     print('todo')
 
 
@@ -52,11 +78,20 @@ if __name__ == "__main__":
             print("getting stats for the stockList stock and writing result!")
             inputExist = True
             stockList, scraped_data = stockGetStats(stockSet)
-            writeStats(stockList, scraped_data)
+            writeStats(stockList, scraped_data, '_custom')
             emailStats(stockList, scraped_data)
 
     if not inputExist:
-        print("screening stock and writing result!")
-        stockList, scraped_data = stockScreen()
-        writeStats(stockList, scraped_data)
-        emailStats(stockList, scraped_data)
+        # check if already exist
+        dateStamp = datetime.datetime.now().strftime("%Y-%m-%d")
+        # script_dir = os.path.dirname(__file__)
+        # abs_path = os.path.join(script_dir, dateStamp)
+        # if not os.path.isdir(abs_path):
+        # not existed
+        if not os.path.isdir('./' + dateStamp):
+            print('existed')
+            print("screening stock and writing result!")
+            stockList, scraped_data = stockScreen()
+            writeStats(stockList, scraped_data)
+            emailStats(stockList, scraped_data)
+        emailStats()
