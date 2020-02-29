ed2eb4c38d5b346a90bfddecf01de5dbcaa34f39
vh2225 <vh2225@columbia.edu>
2020-02-29 02:32:23 -0800

    update import

--- stockUtils.py
+++ stockUtils.py
@@ -1,20 +1,14 @@
 import json
 import re
 import urllib
+import urllib.request
 from collections import OrderedDict
 from time import sleep
+from urllib.request import urlopen
 
 import requests
-import urllib.request
 import urllib3
 from lxml import html
-
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
